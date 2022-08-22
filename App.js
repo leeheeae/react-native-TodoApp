ed3c633cb21f790a60dd4ed29da402656a5cba05
leeheeae <chromeee@daum.net>
2022-08-22 15:54:05 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+import React from 'react';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>TODO APP</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ components/DateHead.js
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function DateHead() {
+  return (
+    <View>
+      <Text>DateHead</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
